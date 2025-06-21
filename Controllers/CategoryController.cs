30aec138850559947f362d7e8248332f08cee8df
Admin <gamevnlc1591@gmail.com>
2025-06-21 15:09:16 +0700

    024 Get Category Details to Edit

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -43,4 +43,21 @@ public class CategoryController : Controller
         }
         return View();
     }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        Category? category = _db.Categories.Find(id);
+        // Category? category1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+        // Category? category2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
 }
